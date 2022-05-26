5071ff4bb4a3826d43caf278df3d81aa08cfe79e
xavier3 <xavjlm@web.de>
2022-05-26 08:12:03 +0200

    cont

--- MESCHACH_VOGLE/VOPL/EXAMPLES/cex3.c
+++ MESCHACH_VOGLE/VOPL/EXAMPLES/cex3.c
@@ -9,7 +9,7 @@
  *
  * 	This one tries to show the various "fit" options
  */
-main()
+int main()
 {
 	char		device[30];
 	static float	x[N] = {
@@ -23,70 +23,80 @@ main()
 			50.0, 20.0
 	};
 
-/*
- *	Get VOGLE device
- */
-	/*
-         printf("Enter VOGLE device: ");
+   /*
+    *	Get VOGLE device
+    */
+	
+   /*
+   printf("Enter VOGLE device: ");
 	gets(device);
-         */
-        vopl_winopen("X11", NULL);
+   */
+   vopl_winopen("X11", NULL);
 
-/*
- *	First we'll do a linear least square fit.
- */
+   /*
+    *	First we'll do a linear least square fit.
+    */
 	fit(2);
 	degree(1);
-/*
- *	Adjust the scaling according to x and y arrays
- */
+
+   /*
+    *	Adjust the scaling according to x and y arrays
+    */
 	adjustscale(x, N, 'x');
 	adjustscale(y, N, 'y');
-/*
- *	Give it a title
- */
+
+   /*
+    *	Give it a title
+    */
 	graphtitle("Linear Least square fit");
-/*
- *	As we are now about to do some graphics we initialise VOGLE
- *	and clear to BLACK
- */
-	
+   
+   /*
+    *	As we are now about to do some graphics we initialise VOGLE
+    *	and clear to BLACK
+    */
 	color(0);
 	clear();
-/*
- *	Draw the title in CYAN
- */
+
+   /*
+    *	Draw the title in CYAN
+    */
 	color(6);
 	drawtitle();
-/*
- *	Now set the color to GREEN
- */
+
+   /*
+    *	Now set the color to GREEN
+    */
 	color(2);
 
-/*
- *	Draw the default set of axes (in GREEN)
- */
+   /*
+    *	Draw the default set of axes (in GREEN)
+    */
 	drawaxes();
-/*
- *	Set color to RED
- */
+   
+   /*
+    *	Set color to RED
+    */
 	color(1);
-/*
- *	Change to the "markers" font and set the current marker string
- */
+
+   /*
+    *	Change to the "markers" font and set the current marker string
+    */
 	font("markers");
 	marker("i");
-/*
- *	Draw the Graph
- */
+
+   /*
+    *	Draw the Graph
+    */
 	plot2(x, y, N);
-/*
- *	Wait around a bit
- */
+
+   /*
+    *	Wait around a bit
+    */
 	getkey();
-/*
- *	Now we'll do a second order fit.
- */
+
+   /*
+    *	Now we'll do a second order fit.
+    */
 	degree(2);
 	graphtitle("Second order least square fit");
 
@@ -95,9 +105,10 @@ main()
 
 	color(7);
 	plot2(x, y, N);
-/*
- *	Change back to the "text" type font to draw the title and axes
- */
+
+   /*
+    *	Change back to the "text" type font to draw the title and axes
+    */
 	font("futura.m");
 
 	color(3);
@@ -105,13 +116,15 @@ main()
 
 	color(6);
 	drawtitle();
-/*
- * 	Wait a bit
- */
+
+   /*
+    * 	Wait a bit
+    */
 	getkey();
-/*
- *	Now do a Cubic spline fit (cardinal spline for this one)
- */
+
+   /*
+    *	Now do a Cubic spline fit (cardinal spline for this one)
+    */
 	fit(3);
 	
 	color(0);
@@ -124,9 +137,9 @@ main()
 	color(6);
 	drawtitle();
 
-/*
- *	Note, we haven't changed to the Marker font here
- */
+   /*
+    *	Note, we haven't changed to the Marker font here
+    */
 	plot2(x, y, N);
 
 	getkey();
--- MESCHACH_VOGLE/VOPL/EXAMPLES/cexcontour.c
+++ MESCHACH_VOGLE/VOPL/EXAMPLES/cexcontour.c
@@ -1,6 +1,5 @@
 /*
  *  cexcontour.c
- *
  */
 
 
